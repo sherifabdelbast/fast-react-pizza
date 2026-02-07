c9dc9cf92a200d08e74d8f72c8dc27c3db974225
sherifabdelbast <sherifabdelbast@yahoo.com>
2026-02-07 21:54:40 +0200

    styling the application using TailwindCSS

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Sherif</div>;
+  return <div className="hidden text-sm font-semibold md:block">Sherif</div>;
 }
 
 export default Username;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,8 +1,8 @@
 import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
-    <div className="my-10 text-center sm:my-16">
-      <h1 className="text-xl mb-8  font-semibold text-center">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-center text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
